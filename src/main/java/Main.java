5fa4f772cbddfbc970aa96761255631819f1e3be
200MB <81110694+200MB@users.noreply.github.com>
2022-06-24 22:40:37 +0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ import java.security.NoSuchAlgorithmException;
 public class Main {
     //todo: add a listener and translator
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, ParseException {
-        LockFileIO lockFileIO = new LockFileIO("C:/Users/bacho/AppData/Local/Riot Games/Riot Client/Config/lockfile");
+        LockFileIO lockFileIO = new LockFileIO("");
         LocalApi api = new LocalApi(lockFileIO);
 
 
